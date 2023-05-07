88f8771d46ff75a39c920df058b96edb30d367c5
illusionist-moon <3232825542@qq.com>
2023-05-07 20:56:46 +0800

    cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,25 +1,40 @@
 package middlewares
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
-
-		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
+	return func(ctx *gin.Context) {
+		method := ctx.Request.Method
+		origin := ctx.Request.Header.Get("Origin")
+		var headerKeys []string
+		for k, _ := range ctx.Request.Header {
+			headerKeys = append(headerKeys, k)
 		}
-		// 处理请求
-		c.Next()
+		headerStr := strings.Join(headerKeys, ",")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		if origin != "" {
+			ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			ctx.Header("Access-Control-Allow-Origin", "*") // 设置允许访问所有域
+			ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+			ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
+			ctx.Header("Access-Control-Max-Age", "172800")
+			ctx.Header("Access-Control-Allow-Credentials", "true")
+			ctx.Set("content-type", "application/json") // 设置返回格式是json
+		}
+		if method == "OPTIONS" {
+			ctx.JSON(http.StatusOK, "Options Request!")
+		}
+		//处理请求
+		ctx.Next()
 	}
 }
