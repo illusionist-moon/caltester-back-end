c5295925bcb2f0c94dbb063b6ad6302a8bcc861b
illusionist-moon <3232825542@qq.com>
2023-05-07 22:49:17 +0800

    cors new

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -13,10 +13,9 @@ func Cors() gin.HandlerFunc {
 			ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			ctx.Header("Access-Control-Allow-Origin", "*") // 设置允许访问所有域
 			ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
-			ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
-			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
+			ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-Custom-Header, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			ctx.Header("Access-Control-Expose-Headers", "Authorization, Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
 			ctx.Header("Access-Control-Max-Age", "172800")
-			ctx.Header("Access-Control-Allow-Credentials", "true")
 			ctx.Set("content-type", "application/json") // 设置返回格式是json
 		}
 		if method == "OPTIONS" {
